f5914e7746f301a237749256fc7b4936dd516b7d
Eirik Torske <eirik.torske@gmail.com>
2011-09-26 10:02:20 +0200

    'MemoizerBuilder' added

---
+++ Memoizer.NET.Test/MemoizerBuilderTests.cs
@@ -0,0 +1,162 @@
+﻿/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Memoizer.NET.Test
+{
+
+    [TestFixture]
+    class MemoizerBuilderTests
+    {
+
+        static readonly Func<long, long> FIBONACCI = (arg => arg <= 1 ? arg : FIBONACCI(arg - 1) + FIBONACCI(arg - 2));
+        //readonly Func<long, long> FIBONACCI2 = (arg => arg <= 1 ? arg : FIBONACCI2(arg - 1) + FIBONACCI2(arg - 2));
+
+
+        [Test]
+        public void SomeTestRunsJustToSeeTheTimeSpent()
+        {
+            long startTime = DateTime.Now.Ticks;
+            long result = (long)FIBONACCI.DynamicInvoke(40);
+            long durationInMilliseconds = (DateTime.Now.Ticks - startTime) / 10000;
+            Console.WriteLine("Fibonacci(40) = " + result + " [non-memoized dynamic invocation took " + durationInMilliseconds + " ms]");
+
+            startTime = DateTime.Now.Ticks;
+            result = FIBONACCI.Invoke(40);
+            durationInMilliseconds = (DateTime.Now.Ticks - startTime) / 10000;
+            Console.WriteLine("Fibonacci(40) = " + result + " [non-memoized invocation took " + durationInMilliseconds + " ms]");
+
+            startTime = DateTime.Now.Ticks;
+            result = (long)FIBONACCI.Memoize().Function.DynamicInvoke(40);
+            durationInMilliseconds = (DateTime.Now.Ticks - startTime) / 10000;
+            Console.WriteLine("Fibonacci(40) = " + result + " [first time 'on-the-spot-memoized' dynamic invocation took " + durationInMilliseconds + " ms]");
+
+            startTime = DateTime.Now.Ticks;
+            result = (long)FIBONACCI.Memoize().Function.DynamicInvoke(40);
+            durationInMilliseconds = (DateTime.Now.Ticks - startTime) / 10000;
+            Console.WriteLine("Fibonacci(40) = " + result + " [second time time 'on-the-spot-memoized' dynamic invocation took " + durationInMilliseconds + " ms]");
+
+            startTime = DateTime.Now.Ticks;
+            result = FIBONACCI.Memoize().Function.Invoke(40);
+            durationInMilliseconds = (DateTime.Now.Ticks - startTime) / 10000;
+            Console.WriteLine("Fibonacci(40) = " + result + " [first time 'on-the-spot-memoized' invocation took " + durationInMilliseconds + " ms]");
+
+            startTime = DateTime.Now.Ticks;
+            result = FIBONACCI.Memoize().Function.Invoke(40);
+            durationInMilliseconds = (DateTime.Now.Ticks - startTime) / 10000;
+            Console.WriteLine("Fibonacci(40) = " + result + " [second time time 'on-the-spot-memoized' invocation took " + durationInMilliseconds + " ms]");
+
+            startTime = DateTime.Now.Ticks;
+            Func<long, long> MEMOIZED_FIBONACCI_1 = FIBONACCI.Memoize().Function;
+            long durationInTicks = DateTime.Now.Ticks - startTime;
+            Console.WriteLine("FIBONACCI.Memoize().Function took " + durationInTicks / 10000 + " ms | " + durationInTicks + " ticks [1 tick ~= 100 ns]");
+
+            startTime = DateTime.Now.Ticks;
+            result = (long)MEMOIZED_FIBONACCI_1.DynamicInvoke(40);
+            durationInTicks = DateTime.Now.Ticks - startTime;
+            Console.WriteLine("Fibonacci(40) = " + result + " [memoized first time dynamic invocation (of memoizer _function_) took " + durationInTicks / 10000 + " ms | " + durationInTicks + " ticks]");
+
+            startTime = DateTime.Now.Ticks;
+            result = (long)MEMOIZED_FIBONACCI_1.DynamicInvoke(40);
+            durationInTicks = DateTime.Now.Ticks - startTime;
+            Console.WriteLine("Fibonacci(40) = " + result + " [memoized second time dynamic invocation (of memoizer _function_) took " + durationInTicks / 10000 + " ms | " + durationInTicks + " ticks]");
+
+            startTime = DateTime.Now.Ticks;
+            result = MEMOIZED_FIBONACCI_1.Invoke(40);
+            durationInTicks = DateTime.Now.Ticks - startTime;
+            Console.WriteLine("Fibonacci(40) = " + result + " [memoized first time invocation (of memoizer _function_) took " + durationInTicks / 10000 + " ms | " + durationInTicks + " ticks]");
+
+            startTime = DateTime.Now.Ticks;
+            result = MEMOIZED_FIBONACCI_1.Invoke(40);
+            durationInTicks = DateTime.Now.Ticks - startTime;
+            Console.WriteLine("Fibonacci(40) = " + result + " [memoized second time invocation (of memoizer _function_) took " + durationInTicks / 10000 + " ms | " + durationInTicks + " ticks]");
+        }
+
+
+
+
+        Func<long, long> slowSquare = (arg1 => { Thread.Sleep(500); return arg1 * arg1; });
+        //Func<long, long> memSlowSquare = slowSquare.Memoize();
+        //long Square(long arg)
+        //{
+        //    return memSlowSquare.Invoke(arg);
+        //}
+
+        [Test]
+        public void ShouldBuildFullBlownMemoizerWithOneliner()
+        {
+            long startTime = DateTime.Now.Ticks;
+            Func<long, long> memoizedSlowSquareFunc = slowSquare.Memoize().Function;
+            long result = memoizedSlowSquareFunc.Invoke(123);
+            long durationInTicks = DateTime.Now.Ticks - startTime;
+            Console.WriteLine("Memoized function construction with invocation: slowSquare.Memoize().Function.Invoke(123)= " + result + " [took " + durationInTicks / 10000 + " ms | " + durationInTicks + " ticks]");
+
+            startTime = DateTime.Now.Ticks;
+            MemoizerBuilder<long, long> slowSquareMemoizerBuilder = slowSquare.Memoize();
+            IInvocable<long, long> memoizedSlowSquare = slowSquareMemoizerBuilder.Get();
+            durationInTicks = DateTime.Now.Ticks - startTime;
+            Console.WriteLine("Memoizer construction: slowSquare.Memoize().Get() took " + durationInTicks / 10000 + " ms | " + durationInTicks + " ticks]");
+
+            startTime = DateTime.Now.Ticks;
+            result = memoizedSlowSquare.InvokeWith(123);
+            durationInTicks = DateTime.Now.Ticks - startTime;
+            Console.WriteLine("Square(123) = " + result + " [memoized first time invocation took " + durationInTicks / 10000 + " ms | " + durationInTicks + " ticks]");
+
+            startTime = DateTime.Now.Ticks;
+            result = memoizedSlowSquare.InvokeWith(123);
+            durationInTicks = DateTime.Now.Ticks - startTime;
+            Console.WriteLine("Square(123) = " + result + " [memoized second time invocation took " + durationInTicks / 10000 + " ms | " + durationInTicks + " ticks]");
+
+
+            slowSquareMemoizerBuilder.InstrumentWith(Console.WriteLine);
+            IInvocable<long, long> memoizedSlowSquare2 = slowSquareMemoizerBuilder.Get();
+
+            startTime = DateTime.Now.Ticks;
+            result = memoizedSlowSquare2.InvokeWith(123);
+            durationInTicks = DateTime.Now.Ticks - startTime;
+            Console.WriteLine("Square(123) = " + result + " [memoized first time (instrumented) invocation took " + durationInTicks / 10000 + " ms | " + durationInTicks + " ticks]");
+
+            startTime = DateTime.Now.Ticks;
+            result = memoizedSlowSquare2.InvokeWith(123);
+            durationInTicks = DateTime.Now.Ticks - startTime;
+            Console.WriteLine("Square(123) = " + result + " [memoized second time (instrumented) invocation took " + durationInTicks / 10000 + " ms | " + durationInTicks + " ticks]");
+
+
+            //startTime = DateTime.Now.Ticks;
+            //IInvocable<long, long> MEMOIZED_FIBONACCI_2 = FIBONACCI.Memoize().Get();
+            ////IInvocable<long, long> MEMOIZED_FIBONACCI_2 = MEMOIZED_FIBONACCI_1.Get(); // or like this
+            //durationInTicks = (DateTime.Now.Ticks - startTime) / 10000;
+            //Console.WriteLine("FIBONACCI.Memoize().Get() took " + durationInTicks + " ticks");
+
+            //IInvocable<long, long> MEMOIZED_FIBONACCI_2 = FIBONACCI.Memoize().LazyLoad().Build();
+
+            //IInvocable<long, long> MEMOIZED_FIBONACCI_3 = FIBONACCI.Memoize().InstrumentWith(Console.WriteLine).Build();
+
+            //IInvocable<long, long> MEMOIZED_FIBONACCI_4 = FIBONACCI.Memoize().InstrumentWith(Console.WriteLine).LazyLoad().Build();
+
+            // "Globalize()" : put he IInvocable in a CLR-wide registry of IInvocable instances - that's eating my own dog food :-)
+            //IInvocable<long, long> MEMOIZED_FIBONACCI_5 = FIBONACCI.Memoize().Build().Globalize();
+
+            //startTime = DateTime.Now.Ticks;
+            //result = MEMOIZED_FIBONACCI_2.InvokeWith(40);
+            //durationInMilliseconds = (DateTime.Now.Ticks - startTime) / 10000;
+            //Console.WriteLine("Fibonacci(40) = " + result + " [memoized first time dynamic invocation took " + durationInMilliseconds + " ms]");
+
+        }
+    }
+}
---
+++ Memoizer.NET/MemoizerBuilder.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Memoizer.NET
+{
+
+    #region Func extension methods
+    public static class FuncExtensionMethods
+    {
+        //public static Func<TParam, TResult> Memoize<TParam, TResult>(this Func<TParam, TResult> functionToBeMemoized)
+        //{
+        //    Memoizer<TResult, TParam> memoizer = new Memoizer<TResult, TParam>(functionToBeMemoized);
+        //    Func<TParam, TResult> proxyFunction = new Func<TParam, TResult>(delegate(TParam arg)
+        //    {
+        //        //return functionToBeMemoized.Invoke(arg);
+        //        //return (TResult)functionToBeMemoized.DynamicInvoke(arg);
+        //        return memoizer.InvokeWith(arg);
+        //    });
+
+        //    return proxyFunction;
+        //}
+
+        //// TODO: memoize/cache the memoized function as well :-D
+        //public static Func<TParam, TResult> Memoize<TParam, TResult>(this Func<TParam, TResult> functionToBeMemoized)
+        //{
+        //    Memoizer<TResult, TParam> memoizer = new Memoizer<TResult, TParam>(functionToBeMemoized);
+        //    Func<TParam, TResult> proxyFunction =
+        //        new Func<TParam, TResult>(delegate(TParam arg)
+        //            {
+        //                return memoizer.InvokeWith(arg);
+        //            });
+        //    return proxyFunction;
+        //}
+        //public static Func<TParam1, TParam2, TResult> Memoize<TParam1, TParam2, TResult>(this Func<TParam1, TParam2, TResult> functionToBeMemoized)
+        //{
+        //    return new Memoizer<TResult, TParam1, TParam2>(functionToBeMemoized).InvokeWith;
+        //}
+        ////public static Func<TParam1, TParam2, TParam3, TResult> Memoize<TParam1, TParam2, TParam3, TResult>(this Func<TParam1, TParam2, TParam3, TResult> functionToBeMemoized) { return new Memoizer<TResult, TParam1, TParam2, TParam3>(functionToBeMemoized).InvokeWith; }
+        ////public static Func<TParam1, TParam2, TParam3, TParam4, TResult> Memoize<TParam1, TParam2, TParam3, TParam4, TResult>(this Func<TParam1, TParam2, TParam3, TParam4, TResult> functionToBeMemoized) { return new Memoizer<TResult, TParam1, TParam2, TParam3, TParam4>(functionToBeMemoized).InvokeWith; }
+
+        public static MemoizerBuilder<TResult, TParam> Memoize<TParam, TResult>(this Func<TParam, TResult> functionToBeMemoized)
+        {
+            return new MemoizerBuilder<TResult, TParam>(functionToBeMemoized);
+        }
+    }
+    #endregion
+
+    //#region Memoizer extension methods
+    //public static class MemoizerExtensionMethods
+    //{
+    //    public static Memoizer<TParam, TResult> InstrumentWith<TParam, TResult>(this Memoizer<TParam, TResult> memoizerToInstrument, Action<String> instrumenter)
+    //    {
+    //        memoizerToInstrument.LoggingMethod = instrumenter;
+    //        return memoizerToInstrument;
+    //    }
+    //}
+    //#endregion
+
+    public class MemoizerBuilder<TResult, TParam>
+    {
+
+        //public static Func<Memoizer<TResult, TParam>, long> newMemoizer =
+        //    new Func<Memoizer<TResult, TParam>, long> {};
+
+        //public static LazyMemoizer<Memoizer<TResult, TParam>, long> memoizerMemoizer = new LazyMemoizer<Memoizer<TResult, TParam>, long>();
+
+        Action<String> LoggingMethod;
+        public MemoizerBuilder(Func<TParam, TResult> functionToBeMemoized) { Function = functionToBeMemoized; }
+        public Func<TParam, TResult> Function { get; private set; }
+        public Memoizer<TResult, TParam> Get()
+        {
+            Memoizer<TResult, TParam> memoizer = new Memoizer<TResult, TParam>(Function);
+            
+            //Memoizer<TResult, TParam> memoizer = memoizerMemoizer.InvokeWith(Memoizer<TResult, TParam>(Function));
+            
+            if (this.LoggingMethod != null) { memoizer.InstrumentWith(this.LoggingMethod); }
+            return memoizer;
+
+        }
+        public void InstrumentWith(Action<String> loggingAction) { this.LoggingMethod = loggingAction; }
+    }
+
+    //class MemoizerReadyForBuilding<TResult, TParam>
+    //{
+    //    Memoizer<TResult, TParam> memoizer;
+
+    //    MemoizerReadyForBuilding(){}
+
+    //    public Memoizer<TResult, TParam> Get()
+    //    {
+    //        return this.memoizer;
+    //    }
+    //}
+
+}
